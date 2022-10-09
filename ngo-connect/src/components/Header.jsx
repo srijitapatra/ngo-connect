e6be6f8b4f0ec33897bc60bdd7980bf30d0cb581
srijita patra <crce.9221.cs@gmail.comgit config --global user.email crce.9221.cs@gmail.comgit config --global user.email crce.9221.cs@gmail.com>
2022-10-09 16:10:52 +0530

    Add Header component for learning

---
+++ ngo-connect/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+function Header(){
+    
+    let title = "sriju";
+
+    return(
+        <>
+        <div style={{backgroundColor : "red"}}>
+        <h1>{title}</h1>
+        </div>
+        </>
+    )
+}
+
+export default Header;
